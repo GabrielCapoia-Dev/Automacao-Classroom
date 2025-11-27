0613a8da9df35b96d60e2d672b57412900873af3
GabrielCapoia-Dev <gabrielcapoia11@gmail.com>
2025-11-27 15:25:26 -0300

    Adaptações para o projeto

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,21 +3,13 @@
 namespace App\Providers;
 
 use App\Models\DominioEmail;
-use App\Models\Escola;
 use App\Models\Permission;
-use App\Models\Aluno;
 use App\Models\Role;
-use App\Models\Serie;
-use App\Models\Turma;
 use App\Models\User;
 use App\Policies\DominioEmailPolicy;
-use App\Policies\EscolaPolicy;
 use App\Policies\PermissionPolicy;
 use App\Policies\RolePolicy;
-use App\Policies\SeriePolicy;
-use App\Policies\TurmaPolicy;
 use App\Policies\UserPolicy;
-use App\Policies\AlunoPolicy;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
 use Filament\Support\Assets\Css;
@@ -40,10 +32,6 @@ class AppServiceProvider extends ServiceProvider
         Gate::policy(Role::class, RolePolicy::class);
         Gate::policy(Permission::class, PermissionPolicy::class);
         Gate::policy(DominioEmail::class, DominioEmailPolicy::class);
-        Gate::policy(Serie::class, SeriePolicy::class);
-        Gate::policy(Escola::class, EscolaPolicy::class);
-        Gate::policy(Turma::class, TurmaPolicy::class);
-        Gate::policy(Aluno::class, AlunoPolicy::class);
         Gate::define('admin-only', function ($user) {
             return $user->hasRole('Admin');
         });
