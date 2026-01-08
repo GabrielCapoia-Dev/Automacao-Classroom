1c9822b33a21b1798ec7adc6fb923c8eccbc31ed
GabrielCapoia-Dev <gabrielcapoia11@gmail.com>
2026-01-08 13:45:56 -0300

    Adiciona rotas para conexão e callback da conta principal do Google

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\GoogleAuthController;
+use App\Http\Controllers\GoogleMainAuthController;
 use Illuminate\Support\Collection;
 use Barryvdh\DomPDF\Facade\Pdf;
 use Illuminate\Support\Str;
@@ -18,3 +19,9 @@ Route::get('/test', function () {
 Route::get('/oauth/redirect/google', [GoogleAuthController::class, 'redirect'])->name('google.redirect');
 Route::get('/oauth/callback/google', [GoogleAuthController::class, 'callback'])->name('google.callback');
 
+// conta MAIN do sistema
+Route::get('/settings/google/connect', [GoogleMainAuthController::class, 'redirect'])
+    ->name('google.main.connect');
+
+Route::get('/settings/google/callback', [GoogleMainAuthController::class, 'callback'])
+    ->name('google.main.callback');
